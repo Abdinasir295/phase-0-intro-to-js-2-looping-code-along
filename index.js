7b2c639a0ed6cad00ab6bbd3d308cd1c24c248e0
Abdinasir295 <abdinasirmehdi62@gmail.com>
2024-04-01 21:28:53 +0300

    updated

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solutions in this file
+    const names=["Charlie", "Samip", "Ali"]
+    function writeCards(names, birthday) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${birthday} gift!`);
+  } 
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
